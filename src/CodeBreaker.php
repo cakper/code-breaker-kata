0121c05704d260bbf9d22a07b6c8c442ec43d5b6
Kacper Gunia <cakper@gmail.com>
2013-06-29 11:26:42 +0100

    Code Breaker Refactor

--- src/CodeBreaker.php
+++ src/CodeBreaker.php
@@ -27,8 +27,9 @@ class CodeBreaker
      */
     public function guess($guess)
     {
+        $allMatches    = $this->countAllMatches($guess);
         $exactMatches  = $this->countExactMatches($guess);
-        $numberMatches = $this->countNumberMatches($guess);
+        $numberMatches = $allMatches - $exactMatches;
 
         return sprintf('%s%s',
             str_repeat(self::EXACT_MATCH_SIGN, $exactMatches),
@@ -44,33 +45,12 @@ class CodeBreaker
      *
      * @return int
      */
-    private function countExactMatches($guess)
+    private function countAllMatches($guess)
     {
-        $exactMatches = 0;
+        $secretChars = str_split($this->secret);
+        $guessChars  = str_split($guess);
 
-        list($secretChars, $guessChars) = $this->getSecretAndGuessChars($guess);
-
-        foreach ($secretChars as $key => $secretChar) {
-            if ($this->isExactMatch($key, $secretChar, $guessChars)) {
-                $exactMatches++;
-            }
-        }
-
-        return $exactMatches;
-    }
-
-    /**
-     * Returns if match is an exact match
-     *
-     * @param $key
-     * @param $secretChar
-     * @param $guessChars
-     *
-     * @return bool
-     */
-    private function isExactMatch($key, $secretChar, $guessChars)
-    {
-        return $secretChar === $guessChars[$key];
+        return count(array_intersect($secretChars, $guessChars));
     }
 
     /**
@@ -80,76 +60,11 @@ class CodeBreaker
      *
      * @return int
      */
-    private function countNumberMatches($guess)
-    {
-        $numberMatches = 0;
-
-        list($secretChars, $guessChars) = $this->getSecretAndGuessChars($guess);
-
-        foreach ($secretChars as $key => $secretChar) {
-            if ($this->isExactMatch($key, $secretChar, $guessChars)) {
-                $this->unsetExactMatch($key, $secretChars, $guessChars);
-            } elseif ($this->isNumberMatch($key, $secretChars, $guessChars)) {
-                $numberMatches++;
-                $this->unsetNumberMatch($key, $secretChars, $guessChars);
-            }
-        }
-
-        return $numberMatches;
-    }
-
-    /**
-     * Unset exact match in both arrays
-     *
-     * @param integer $key         Current key
-     * @param array   $secretArray Array of secret chars
-     * @param array   $guessArray  Array of guess chars
-     */
-    private function unsetExactMatch($key, array &$secretArray, array &$guessArray)
-    {
-        unset($secretArray[$key]);
-        unset($guessArray[$key]);
-    }
-
-    /**
-     * Returns if given char exists in secret array
-     *
-     * @param integer $key         Current key
-     * @param array   $secretArray Array of secret chars
-     * @param array   $guessArray  Array of guess chars
-     *
-     * @return bool
-     */
-    private function isNumberMatch($key, array $secretArray, array $guessArray)
-    {
-        return (in_array($guessArray[$key], $secretArray) !== false);
-    }
-
-    /**
-     * Unset given char in Secret and guest array
-     *
-     * @param integer $key         Current key
-     * @param array   $secretArray Array of secret chars
-     * @param array   $guessArray  Array of guess chars
-     */
-    private function unsetNumberMatch($key, array &$secretArray, array &$guessArray)
-    {
-        unset($secretArray[array_search($guessArray[$key], $secretArray)]);
-        unset($guessArray[$key]);
-    }
-
-    /**
-     * Returns array of Secret and Guess arrays
-     *
-     * @param string $guess User guess
-     *
-     * @return array
-     */
-    private function getSecretAndGuessChars($guess)
+    private function countExactMatches($guess)
     {
         $secretChars = str_split($this->secret);
         $guessChars  = str_split($guess);
 
-        return array($secretChars, $guessChars);
+        return count(array_intersect_assoc($secretChars, $guessChars));
     }
 }
